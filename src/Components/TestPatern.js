5dd6a826d243f7a36a25e485c691d1ad6f0bbc60
Stanislav Tsyplakov <32098655+MrRufRUS@users.noreply.github.com>
2021-03-17 18:34:23 +0700

    Test system
    The development of test system was started

---
+++ src/Components/TestPatern.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import questions from './Questions'
+
+class Quiz extends React.Component {
+    state = {
+        userAnswer: null,
+        currentQuestions: 0,
+        options: []
+    }
+}
+
+function readQuestions() {
+    const questions = 
+}
+
+function TestPatern() {
+    return(
+        
+    )
+}
